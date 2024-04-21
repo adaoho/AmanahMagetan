91b951b133f58a6f098c72e54926f7f7c2358c4d
adaoho <9nugroho@gmail.com>
2024-04-22 06:14:15 +0700

    Initial Project Base

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="w-screen h-screen flex justify-center items-center bg-white">
+        <div className="w-[428px] h-screen flex justify-center items-center bg-red-400"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
